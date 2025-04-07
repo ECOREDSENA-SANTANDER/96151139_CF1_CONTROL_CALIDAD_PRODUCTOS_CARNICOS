46aed2331ff1e54255bcd51be6d15b374d10f110
Andrea Botello <andrea.botellor@misena.edu.co>
2025-04-07 11:33:36 -0500

    Tema 2 e imagenes Tema 1

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -5,9 +5,93 @@
       .titulo-principal.color-acento-contenido
         .titulo-principal__numero
           span 1
-        h1 Titulo de primer nivel
+        h1 Generalidades de la carne
     
+      .row.justify-content-center.align-items-center.mb-5
+        .col-lg-5(data-aos="fade-right")
+          img(src='@/assets/curso/temas/t1/1.png', alt='')
+        .col-lg-7(data-aos="fade-left")
+          p La carne se define como la porción del tejido muscular de los animales que puede ser consumida por los seres humanos como alimento. Se considera carne al músculo de diversos tipos de animales, generalmente vertebrados terrestres, es decir, mamíferos, aves e incluso algunos peces.
+          .row.justify-content-center.align-items-center.mb-4
+            .col-lg-2
+              img(src='@/assets/curso/temas/t1/2.svg', alt='')
+            .col-lg-10
+              p.mb-0 La carne es una de las fuentes de alimento con mayor valor nutricional para quienes la consumen, ya que en su composición química contiene una serie de elementos y compuestos que aportan nutrientes al organismo. Entre ellos se encuentran las proteínas, las grasas, los carbohidratos, los minerales y las vitaminas, entre otros.
+          p Hoy en día, los productos derivados de la carne animal son altamente valorados en los países desarrollados, ya que cuentan con una alta demanda por parte de los consumidores.
+
+      .subt.mb-4(data-aos="fade-right")
+        .row.align-items-center
+          .col-auto
+            img(src='@/assets/curso/temas/t2/subt.svg', alt='')
+          .col-auto
+            h3.mb-0.text-white Clasificación general de la carne
+
+      .row.justify-content-center.align-items-center.mb-5
+        .col-lg-9
+          p Las carnes se clasifican principalmente según el tipo de animal del que provienen, sin embargo, la clasificación más importante y conocida a nivel mundial es la que distingue entre carnes rojas y carnes blancas, ya que incluye diferentes variedades. Por ejemplo, dentro de las carnes rojas se encuentran la de res, cerdo y buey; mientras que las carnes blancas corresponden a las que provienen de las aves y los peces.
+        .col-lg-3
+          img(src='@/assets/curso/temas/t1/3.svg', alt='')
+
+      .row.bgc-3.align-items-center.p-3.mb-5(data-aos="fade-down")
+        .col-lg-12.col-12.px-lg-4
+          .row.justify-content-center.align-items-center
+
+            .row.justify-content-center.mb-4
+              .col-12
+                .subt2.mb-4
+                  h3.mb-0 • Carnes rojas
+              .col-lg-5
+                p Las carnes rojas reciben esta denominación porque generalmente presentan un color rojizo o rosado. En cuanto a las fuentes de carne correspondientes a esta categoría se incluyen las carnes que provienen de los mamíferos, ya que la mayoría de ellas entran bajo esta clasificación.
+                p Las carnes rojas no constituyen técnicamente una clasificación científica, sino que corresponden a una denominación de uso culinario. Esto implica que puedan presentarse algunas excepciones respecto a cuáles animales proveen carne roja. Sin embargo, de manera general, se consideran todos los mamíferos, especialmente el ganado bovino. También se incluyen la carne de cerdo, toro, venado, jabalí, cordero y otros animales de caza.
+              .col-lg-4
+                .titulo-sexto.color-acento-contenido
+                  h5 Figura 1.
+                  span Cortes de carne roja
+                figure  
+                  img(src='@/assets/curso/temas/t1/fig1.png', alt='La figura presenta cinco filetes de carne roja cruda dispuestos en abanico uno sobre el otro y sobre una superficie blanca. Los filetes están acompañados de otros ingredientes como ajo, cebolla, tomate, hojas y un recipiente de madera con una mezcla de pimientas en grano.')  
+                figcaption #[b Fuente:] (Chabraszewski, s.f.)
+              .col-lg-3
+                p El color rojo característico de esta variedad de carne se debe a la presencia de una proteína llamada mioglobina. Además de esta, también se encuentran sustancias conocidas como purinas, las cuales se descomponen en ácido úrico, un residuo del metabolismo en el cuerpo humano. Sin embargo, se ha comprobado que las carnes rojas, al contener estas sustancias en mayor proporción que otros alimentos, exigen un mayor esfuerzo del sistema digestivo. Por esta razón, su consumo excesivo puede ocasionar perjuicios para la salud.
+
+            .row.justify-content-center.align-items-center.mb-5
+              .col-lg-3
+                img(src='@/assets/curso/temas/t1/4.svg', alt='')
+              .col-lg-9
+                p Las carnes rojas contienen un alto porcentaje de grasas saturadas, lo que puede contribuir a la acumulación de colesterol de baja densidad (conocido como colesterol "malo") en las arterias. Esta situación incrementa el riesgo de desarrollar enfermedades cardíacas. Por esta razón, el consumo de carnes rojas es, en ocasiones, considerado un hábito poco saludable, lo que ha llevado a algunas personas a reducir su ingesta o incluso eliminar este alimento de su dieta.
+            
+            p.mb-0 Sin embargo, las carnes rojas son una fuente valiosa de nutrientes esenciales y proteínas de alta calidad, que aportan beneficios al metabolismo y al funcionamiento general del organismo. La clave está en mantener un consumo moderado y equilibrado. El abuso en su ingesta puede generar riesgos para la salud, pero cuando se incluyen en una dieta balanceada, las carnes rojas pueden contribuir positivamente a una alimentación saludable.
+
+      .subt2.mb-4(data-aos="fade-right")
+        h3.mb-0 • Carnes blancas
       p.mb-4 Las carnes blancas se consideran la contraparte de las carnes rojas, esta clasificación incluye las carnes que provienen de animales como las aves (pollo, pavo, pato, ganso) y algunos mamíferos como el conejo. 
+
+      .row.justify-content-center
+        .col-lg-4(data-aos="fade-right")
+          .titulo-sexto.color-acento-contenido
+            h5 Figura 2.
+            span Cortes de carne blanca
+          figure  
+            img(src='@/assets/curso/temas/t1/fig2.png', alt='La figura presenta dos trozos de carne blanca cruda que supone ser pechuga de pollo colocados uno al lado del otro. ')  
+          figcaption #[b Fuente:] (Chabraszewski, s.f.)
+        .col-lg-8(data-aos="fade-left")
+          p Por lo general, se considera que las carnes blancas son más saludables que las carnes rojas, esto se debe a que su contenido de grasas y calorías es menor, lo que las convierte en una opción más ligera y recomendada en muchas dietas equilibradas.
+          p Si bien esto es cierto, lo saludable de la carne no depende siempre de que sea roja o blanca, debido a que el contenido de grasas y calorías depende muchas veces de la parte del animal que se esté consumiendo. Así pues, una parte especifica como el lomo de un animal vacuno puede resultar más saludable en cuestión de grasas y calorías que un ala de pollo con piel. Por tanto, no siempre la carne blanca es mejor que la roja, ya que incluso en el caso mencionado, el ala del pollo posee una mayor cantidad de colesterol de baja densidad o colesterol “malo”.
+          .row.justify-content-center.align-items-center
+            .col-lg-8
+              p La procedencia de las carnes blancas destinadas al consumo humano en los diferentes mercados a nivel mundial suele estar ligada, generalmente, a la producción industrial de animales domesticados. Por esta razón, existen diversas variedades de carnes que dependen de las zonas geográficas de donde provienen.
+            .col-lg-4
+              img(src='@/assets/curso/temas/t1/5.svg', alt='')  
+
+
+      Separador
+
+      #t_1_1.titulo-segundo.color-acento-contenido(data-aos='fade-right')
+        h2 1.1 Clasificación de la carne según el origen
+
+
+      p.mb-5 La carne se clasifica de acuerdo con el animal del que proviene, lo que influye en sus características nutricionales y sensoriales. Conocer el origen de la carne permite entender sus propiedades nutricionales, cómo procesarla, cómo controlar y conservar su calidad y cómo integrarla en una dieta equilibrada o en determinado mercado. 
+
+
 
   </template>
 
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -112,7 +112,7 @@
                         li 
                           i.fas.fa-check-circle
                           | Hongos en la parte externa.
-                .col-lg-4.col-md-5
+                .col-lg-4.col-md-5.col-9
                   img(src='@/assets/curso/temas/t2/slide3.png', alt='')
               .row.justify-content-center
                 .col-lg-7
@@ -137,7 +137,7 @@
                           i.fas.fa-check-circle
                           | Tocinetas crudas.
                   p Se puede afirmar que muchos de estos productos pueden tener una vida útil larga de aproximadamente 18 meses sin refrigeración y que su calidad está ligada a la materia prima usada para su elaboración. 
-                .col-lg-4.col-md-5
+                .col-lg-4.col-md-5.col-9
                   img(src='@/assets/curso/temas/t2/slide4.png', alt='')
               .row.justify-content-center
                 .col-lg-7
@@ -199,16 +199,16 @@
 
     .row.justify-content-center.mb-4.img-rara(data-aos="fade-down")
       .row.justify-content-center.mb-4
-        .col-lg-8
+        .col-lg-8.mb-3.mb-lg-0
           p En la elaboración de productos cárnicos se usan diversos ingredientes que cumplen funciones específicas para garantizar la calidad, la aceptación del producto final y la seguridad alimentaria. Los ingredientes no solo aportar al sabor, al color o a la textura, sino que también contribuyen a la conservación y estabilidad del alimento. 
           .d-flex.justify-content-end
             .col-lg-8.ps-lg-0
-              .bg4.p-4.py-3
+              .bg4.p-4.py-3.ms-lg-4
                 p.mb-0 Según la NTC 1325 de 2023, los ingredientes de formulación de productos cárnicos son los productos que, sin ser indispensables para conferir identidad al producto terminado, pueden ser usados sin limitaciones directas de dosificación, excepto por su efecto en la composición del producto final.
-        .col-lg-4
+        .col-lg-4.col-md-8
           img(src='@/assets/curso/temas/t2/3.png', alt='')
       .row.justify-content-center
-        .col-lg-3
+        .col-lg-3.d-none.d-lg-block
           img.la-img(src='@/assets/curso/temas/t2/2.png', alt='')
         .col-lg-9
           p Por otro lado, los aditivos son sustancias que normalmente no se consumen como alimento, ni se usan normalmente como ingrediente característico del alimento y cuya adición intencional al alimento es con un fin tecnológico e incluso organoléptico. Es decir, los aditivos son adicionados a los alimentos para potenciar el sabor, preservar el alimento, lograr determinadas características organolépticas, prolongar su vida útil en envasado, almacenamiento y transporte. 
@@ -228,7 +228,178 @@
                 i.fas.fa-check-circle
                 | Contribuir al proceso de producción, envasado, almacenamiento y transporte.
 
-    p Los aditivos para productos procesados que son enlatados están regidos por el Food Chemical Codex (FCC), que establece las especificaciones de pureza y calidad.
+    p.mb-5 Los aditivos para productos procesados que son enlatados están regidos por el Food Chemical Codex (FCC), que establece las especificaciones de pureza y calidad.
+
+    .subt.mb-4(data-aos="fade-right")
+      .row.align-items-center
+        .col-auto
+          img(src='@/assets/curso/temas/t2/subt.svg', alt='')
+        .col-auto
+          h3.mb-0.text-white Ingredientes no cárnicos
+
+    TabsA.color-acento-botones.mb-5(data-aos="fade-down")
+      .tarjeta.color-primario.p-4(titulo="a. Agua o hielo")
+        h4 Agua o hielo
+        p El agua o el hielo es un ingrediente muy importante en la producción cárnica, puesto que permite dar suavidad y jugosidad a los productos cárnicos, porque gracias a su bipolaridad de cargas se fija con facilidad a las proteínas.
+        p Las funciones principales del agua y del hielo son: 
+        ul.lista-ul--color.fa-lista
+          li.mb-1
+            i.fas.fa-check-circle
+            | El agua ayuda a disolver la sal y los demás ingredientes adicionados en los productos cárnicos.
+          li.mb-1
+            i.fas.fa-check-circle
+            | El hielo ayuda a estabilizar la emulsión cárnica, porque mantiene una temperatura baja y constante. 
+          li.mb-0
+            i.fas.fa-check-circle
+            | Desde el punto de vista económico, el agua disminuye los costos en el proceso de elaboración. 
+        .bg1.tarjeta.p-3.px-md-4 
+          .row.justify-content-center.align-items-center
+            .col-lg-2.col-md-2.col-3.mb-3.mb-md-0
+              img.px-lg-2(src='@/assets/curso/temas/t2/tab1.svg', alt='')
+            .col-lg-10.col-md-10
+              p.mb-0 #[b Nota.] El agua o hielo que es utilizado en la elaboración de productos cárnicos debe ser agua de alta calidad química, higiénica y sanitaria, libre de cualquier agente contaminante ya sea químico o biológico, dado el uso alimentario al que va a ser destinada. Por tal razón, es indispensable que las plantas de producción de derivados cárnicos cuenten con agua potable. 
+      .tarjeta.color-primario.p-4(titulo="b. Sal")
+        h4 Sal
+        p La sal común o cloruro de sodio es un compuesto que se viene usando desde tiempos remotos en el procesamiento de la carne. Es un compuesto altamente higroscópico pues tiene la capacidad de reducir la actividad de agua de los alimentos facilitando la conservación y contribuyendo al desarrollo de los sabores. 
+        p Las funciones de la sal en la elaboración de productos cárnicos son:  
+        ul.lista-ul--color.fa-lista
+          li.mb-1
+            i.fas.fa-check-circle
+            span.mb-0 Dar el sabor salado a los productos cárnicos, generalmente se debe adicional el 2.5 % de sal al producto. 
+          li.mb-1
+            i.fas.fa-check-circle
+            span.mb-0 Proporcionar un efecto bacteriostático especialmente contra los microorganismos como las bacterias coliformes. Cuando la sal es usada por encima del 5 % contribuye a la inhibición de microorganismos anaerobios como la bacteria #[em Clostridium botulinum].
+          li.mb-1
+            i.fas.fa-check-circle
+            | Aumentar la capacidad de retención de agua de la carne. 
+          li.mb-1
+            i.fas.fa-check-circle
+            | Bajar el punto isoeléctrico de las proteínas de la carne generando un entorno en que se altera la carga de las mismas, disminuyendo el punto donde estas pierden su capacidad de retención de agua.
+          li.mb-0
+            i.fas.fa-check-circle
+            | Solubilizar las proteínas, mejorando su capacidad de formar emulsiones estables. 
+        .bg1.tarjeta.p-3.px-lg-4 
+          .row.justify-content-center.align-items-center
+            .col-lg-2.col-md-2.col-4.mb-3.mb-md-0
+              img(src='@/assets/curso/temas/t2/tab2.svg', alt='')
+            .col-lg-10.col-md-10
+              p.mb-0 #[b Nota.]  La sal debe ser de alta pureza para evitar que los productos cárnicos se oxiden y/o contaminen, además se recomienda almacenarla en lugares secos debido a su capacidad para captar la humedad del ambiente.  
+      .tarjeta.color-primario.p-4(titulo="c. Azúcar")
+        h4 Azúcar
+        p En los productos cárnicos se usa el azúcar o sacarosa extraído de la caña azucarera, también se usa la dextrosa o glucosa que se deriva del maíz, además de otros jarabes y edulcorantes como el sorbitol. 
+        p Las funciones del azúcar en los cárnicos son:
+        ul.lista-ul--color.fa-lista
+          li.mb-1
+            i.fas.fa-check-circle
+            span.mb-0 Ayudar en la fermentación con microorganismos benéficos en los embutidos, actuando como fuente de energía. 
+          li.mb-1
+            i.fas.fa-check-circle
+            span.mb-0 Incrementar el pardeamiento no enzimático durante la cocción de los cárnicos. 
+          li.mb-1
+            i.fas.fa-check-circle
+            span.mb-0 En cantidades o concentraciones altas, puede tener un efecto conservante. 
+          li.mb-1
+            i.fas.fa-check-circle
+            span.mb-0 Mejorar el aroma y el sabor de los productos cárnicos. 
+      .tarjeta.color-primario.p-4(titulo="d. Almidones y proteínas de origen animal y vegetal")
+        h4 Almidones y proteínas de origen animal y vegetal
+        p Los almidones y proteínas de origen tanto animal como vegetal se usan como sustancias ligantes y emulsionantes en los procesos de producción de cárnicos debido a que aumentan la estabilidad de los productos y mejoran su rendimiento. 
+        .row.justify-content-center.align-items-center.mb-4
+          .col-lg-6
+            p Las funciones de estos ingredientes son: 
+            ul.lista-ul--color.fa-lista
+              li.mb-1
+                i.fas.fa-check-circle
+                span.mb-0 Actuar como sustancias ligantes y emulsionantes.
+              li.mb-1
+                i.fas.fa-check-circle
+                span.mb-0 Aumentar la estabilidad de las emulsiones.
+              li.mb-0
+                i.fas.fa-check-circle
+                span.mb-0 Reducir el costo de las formulaciones.
+          .col-lg-6
+            img(src='@/assets/curso/temas/t2/tab3.png', alt='')  
+        p Generalmente son empleadas las harinas de los cereales y los almidones del maíz, trigo, papa y yuca, además de derivados de la soya como la proteína texturizada y derivados de las proteínas de la leche de vaca como el caseinato. 
+        p El caseinato es empleado para disminuir la separación de la grasa en aquellos embutidos escaldados que colocados en envases o recipientes herméticos son sometidos a temperaturas mayores de las habituales. Este ingrediente se obtiene a partir de la lecha desnatada por disgregación con bicarbonato de sodio, citratos o por disgregación mediante hidróxido de sodio, calcio o potasio.
+        p A diferencia de la proteína cárnica, el caseinato no forma una red alrededor de las partículas de grasa en la masa del embutido escaldado, sino que se une directamente a las moléculas de grasa, envolviéndolas con una capa más fina, lo que incrementa la superficie expuesta de la grasa. Esta envoltura no se extiende ni se vuelve más delgada; por el contrario, el caseinato se acumula en la nueva superficie formada, permaneciendo intacta la cobertura.
+      .tarjeta.color-primario.p-4(titulo="e. Especias y condimentos")
+        h4 Especias y condimentos
+        p Las especias pueden ser naturales o en forma de aceites esenciales que se obtienen generalmente de diferentes partes de las plantas como hojas, tallos y semillas. Las especias mejoran y depuran el aroma de los embutidos en virtud de su acción sazonante. 
+        p Las especias se pueden clasificar así:
+        ul.lista-ul--color.fa-lista.columns-2.mb-4
+          li.mb-1
+            i.fas.fa-check-circle
+            span.mb-0 Hojas: laurel, mejorana, romero y tomillo.
+          li.mb-1
+            i.fas.fa-check-circle
+            span.mb-0 Tallos: apio, cilantro, perejil.
+          li.mb-1
+            i.fas.fa-check-circle
+            span.mb-0 Corteza: canela.
+          li.mb-1
+            i.fas.fa-check-circle
+            span.mb-0 Frutas: pimienta y nuez moscada.
+          li.mb-1
+            i.fas.fa-check-circle
+            span.mb-0 Raíces: rábano.
+          li.mb-1
+            i.fas.fa-check-circle
+            span.mb-0 Bulbos: cebolla y ajo.
+          li.mb-1
+            i.fas.fa-check-circle
+            span.mb-0 Semillas: comino y mostaza.
+        p Eligiendo y mezclando muy bien las especias se pueden fabricar productos cárnicos de muy buena calidad olfativa, además de influir en el sabor, llegando a satisfacer las distintas preferencias del consumidor. Se recomienda una adición máxima del 1 %.
+        p Las cantidades recomendadas a utilizar de algunas especias son las siguientes:
+        .row.justify-content-center.align-items-center.mb-4
+          .col-lg-7.mb-3.mb-lg-0
+            p.mb-0 Pimienta blanca: entre 1 y 3 gramos por kilogramo de pasta final. 
+            p.mb-0 Pimienta negra: entre 0.5 y 1 gramos por kilogramo de pasta. 
+            p.mb-0 Ajo en polvo: entre 2 y 3 gramos por kilogramo de pasta. 
+            p.mb-0 Mejorana: entre 0.3 y 2 gramos por kilogramo de pasta.
+            p.mb-0 Romero: entre 0.2 y 0.4 gramos por kilogramo de pasta final.
+            p.mb-0 Comino: entre 1 y 1.5 gramos por kilogramo de pasta final.
+            p.mb-0 Orégano: entre 0.2 y 0.3 gramos por kilogramo de pasta final.
+            p.mb-0 Laurel: entre 0.2 y 0.3 gramos por kilogramo de pasta final.
+            p.mb-0 Tomillo: entre 0.3 y 0.5 gramos por kilogramo de pasta final.
+            p.mb-0 Páprika: entre 1.5 y 3 gramos por kilogramo de pasta final. 
+          .col-lg-5.col-md-7
+            img(src='@/assets/curso/temas/t2/tab4.png', alt='')
+        p Los condimentos son mezclas de especias empleadas para definir el sabor de determinado producto cárnico. 
+        p Ya existen en el país empresas especializadas en la fabricación de condimentos para cada tipo de producto cárnico. Esto permite que las empresas productoras de cárnicos utilicen un condimento estandarizado para determinado producto. Por ejemplo, existen condimentos específicos para la salchicha tipo #[em Frankfurt], el salchichón, la mortadela, el cábano, entre otros. La cantidad de estos condimentos a utilizar dependerá de la recomendación del proveedor, que por lo general es de 8 gramos por kilogramo de pasta cárnica final.
+        p En Colombia los productos vegetales como especias y condimentos son de uso permitido y no hay una dosis tóxica registrada, por esto es importante dar una buena manipulación a estos productos utilizando las normas de Buenas Prácticas de Manufactura (BPM).
+
+
+    .subt.mb-4(data-aos="fade-right")
+      .row.align-items-center
+        .col-auto
+          img(src='@/assets/curso/temas/t2/subt.svg', alt='')
+        .col-auto
+          h3.mb-0.text-white Aditivos
+
+    .row.justify-content-center.mb-5
+      .col-lg-3.d-none.d-lg-block
+        img(src='@/assets/curso/temas/t2/5.png', alt='')
+      .col-lg-9
+        AcordionA(tipo="a" clase-tarjeta="tarjeta bg-color-primario")
+          .row.justify-content-center.text-white(titulo="a. Nitritos y nitratos")
+            .col-md-11.col-12
+              p Los nitritos y nitratos son llamadas también sales de curación y desde los antepasados se han utilizado para conservar los alimentos, evitando la proliferación de bacterias como la Clostridium botulinum, además gracias a una reacción química parcial con la mioglobina de la carne dan color rosado característico de los productos cárnicos. 
+              p Estas sustancias son muy usadas en la elaboración de productos de corta maduración como las salchichas, los jamones, las mortadelas, entre otros, para prevenir el crecimiento de bacterias, estabilizar el color y resaltar el sabor. 
+              p De acuerdo con la NTC 1325 de 2023 los nitratos de sodio y de potasio pueden ser usados únicamente en productos madurados o fermentados o ambos en un máximo de 200 miligramos por kilogramo. Y en el caso de los nitritos de sodio o de potasio, estos pueden ser usados en máximo 200 miligramos por kilogramo.
+          .row.justify-content-center.text-white(titulo="b. Fosfatos")
+            .col-md-11.col-12
+              p En la industria de las carnes, los fosfatos son utilizados principalmente para aumentar la capacidad de retención de humedad de los productos y formar emulsiones estables. Gracias a su elevado poder de solubilizar las proteínas miofibrilares, responsables de la ligazón muscular, contribuyen a desarrollar una estructura agradable en los productos cárnicos. 
+              p Su principal función en las carnes procesadas está relacionada con la acción sobre el pH, lo que contribuye a conferir una estructura elástica semejante a la encontrada en los animales recién sacrificados. 
+          .row.justify-content-center.text-white(titulo="c. Acido glutámico de sodio")
+            .col-md-11.col-12
+              p El ácido glutámico de sodio, también conocido como glutamato monosódico (GMS) es un aditivo alimentario que se utiliza para potenciar el sabor de los alimentos procesados en general. En el año 1908 se le aisló por primera vez en Japón y ahora es empleado en todo el mundo como potenciador del sabor.
+              p Los potenciadores del sabor son sustancias que, sin modificar el sabor propio del producto, exaltan la percepción olfato-gustativa de ese sabor. El mecanismo por el que se produce este fenómeno no está totalmente claro. Por un lado, parece ser que actúan directamente sobre las terminaciones nerviosas haciéndolas especialmente sensibles a los sabores, pero por otro lado se entiende que no tienen efecto alguno sobre los cuatro sabores de base (dulce, salado, ácido y amargo). 
+          .row.justify-content-center.text-white(titulo="d. Antioxidantes")
+            .col-md-11.col-12
+              p Los antioxidantes son sustancias que pueden alargar la vida útil de un producto evitando el enranciamiento de las grasas y el cambio de color por oxidación.  
+              p En las industrias cárnica se emplean el Butilhidroxianisol (BHA) y el Butilhidroxitolueno (BHT), los cuales actúan retardando la rancidez cárnica. De acuerdo con la NTC 1325 de 2023 se permite utilizar el 0.01 % en peso, máximo referido al contenido de grasa en productos frescos y el 0.003 % en peso máximo en productos deshidratados. 
+
+
 
 </template>
 
