1d92a223d7867386fcb65a9f46c0d9cf0dea3730
Andrea Botello <andrea.botellor@misena.edu.co>
2025-04-07 19:01:44 -0500

    Avance Tema 1

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -90,7 +90,7 @@
 
       p.mb-5 La carne se clasifica de acuerdo con el animal del que proviene, lo que influye en sus características nutricionales y sensoriales. Conocer el origen de la carne permite entender sus propiedades nutricionales, cómo procesarla, cómo controlar y conservar su calidad y cómo integrarla en una dieta equilibrada o en determinado mercado. 
 
-      .subt.mb-4(data-aos="fade-left")
+      .subt.mb-4(data-aos="fade-right")
         .row.align-items-center
           .col-auto
             img(src='@/assets/curso/temas/t2/subt.svg', alt='')
@@ -144,7 +144,7 @@
               h4.bg-color-acento-contenido.text-center.text-white.py-1 Vacuno mayor: 
               p.text-center En esta variedad de carne se encuentra la de la vaca y el toro mayor de 48 meses (4 años). Las carnes procedentes de este grupo resultan con una menor ternura y poseen el más alto contenido graso comparado con las demás variedades, lo que también las hace más jugosas. Además, poseen un elevado valor nutricional, lo que la convierte en uno de los tipos de carne más consumidos y apreciados en la ganadería. 
 
-      .subt.mb-4(data-aos="fade-left")
+      .subt.mb-4(data-aos="fade-right")
         .row.align-items-center
           .col-auto
             img(src='@/assets/curso/temas/t2/subt.svg', alt='')
@@ -178,7 +178,7 @@
           p Adicionalmente, este tipo de carne es una buena fuente de vitaminas y minerales, pues en ella se pueden encontrar las vitaminas del grupo B en especial la vitamina B12 y la B6. 
 
 
-      .subt.mb-4(data-aos="fade-left")
+      .subt.mb-4(data-aos="fade-right")
         .row.align-items-center
           .col-auto
             img(src='@/assets/curso/temas/t2/subt.svg', alt='')
@@ -199,6 +199,179 @@
         .col-lg-9.col-md-8
           p Este tipo de carne también es una fuente de ácidos grasos monoinsaturados, principalmente de ácido oleico (Velasco et al, 2019). Este tipo de ácidos grasos contribuyen a aumentar ligeramente los niveles de colesterol de alta densidad (conocido como colesterol “bueno”), lo que a su vez ayuda a reducir los niveles de colesterol de baja densidad. 
           p Estudios han demostrado que en esencia la carne de cerdo es saludable para las personas (Lizcano & Valenzuela, 2021), sin embargo, siempre se debe ser cuidadoso a la hora de cocinarla, pues es importante que la cocción se realice correctamente, ya que una carne parcialmente cocinada podría contener agentes patógenos causantes de enfermedades.
+
+      .row.bgc-4.align-items-center.p-3
+        .col-lg-12.col-12.px-lg-4
+          .row.justify-content-center.align-items-center
+            
+            .col-12
+              .subt.mb-4(data-aos="fade-right")
+                .row.align-items-center
+                  .col-auto
+                    img(src='@/assets/curso/temas/t2/subt.svg', alt='')
+                  .col-auto
+                    h3.mb-0.text-white Carne de aves
+
+            .row.justify-content-center
+              .col-lg-4(data-aos="fade-right")
+                img(src='@/assets/curso/temas/t1/12.png', alt='')
+              .col-lg-8(data-aos="fade-left")   
+                .row.justify-content-center.align-items-center.mb-4
+                  .col-lg-8
+                    p Las aves son animales que se alimentan de granos o insectos. Su carne está clasificada como carne blanca, y si bien es una carne que se caracteriza por contener una serie de proteínas de alta calidad y grasas que contribuyen al aumento del colesterol “bueno”, también pueden contener grasas poliinsaturadas, que podrían elevar los niveles de colesterol “malo”. 
+                  .col-lg-4 
+                    img(src='@/assets/curso/temas/t1/13.svg', alt='')  
+                p Una de las aves más consumidas a nivel mundial es el pollo, el cual es domesticado y criado industrialmente en grajas de engorde. Estos animales son alimentados con concentrados que favorecen una rápida ganancia de peso, generalmente son sacrificados cuando alcanzan un peso que varía entre 1 y 3 kilogramos. La carne de pollo es económica y de fácil acceso para gran parte de la población, además es muy versátil en ámbitos culinarios, ya que posee un alto valor nutricional y resulta fácil de digerir.     
+
+
+            Separador
+
+            #t_1_2.titulo-segundo.color-acento-contenido(data-aos='fade-right')
+              h2 1.2 Composición química de la carne 
+
+            .row.justify-content-center.align-items-center.mb-5
+              .col-lg-8(data-aos="fade-right")
+                p A nivel de la composición química puede afirmarse que la carne se encuentra constituida por diversos compuestos. La mayor parte de su composición corresponde al agua, la cual representa más del 75 % del total de la carne; le siguen las proteínas, que constituyen aproximadamente el 20 %. 
+                .bg3.p-3.px-lg-4
+                  .row.justify-content-center.align-items-center
+                    .col-lg-2
+                      img.px-lg-2(src='@/assets/curso/temas/t1/14.svg', alt='') 
+                    .col-lg-10
+                      p.mb-0 El contenido de grasa es muy variable, desde un 3 % a un 30 % de su composición distribuida en grasa intermuscular, grasa intramuscular y subcutánea. La grasa intramuscular o marmoleo suele ser menor al 3 %; el porcentaje restante está conformado por sales minerales, vitaminas y carbohidratos.  
+              .col-lg-4(data-aos="fade-left")
+                img(src='@/assets/curso/temas/t1/15.png', alt='') 
+
+      .subt.mb-4(data-aos="fade-right")
+        .row.align-items-center
+          .col-auto
+            img(src='@/assets/curso/temas/t2/subt.svg', alt='')
+          .col-auto
+            h3.mb-0.text-white A. Agua
+      
+      .row.justify-content-center.mb-4(data-aos="fade-down")
+        .col-lg-4
+          p El agua es la sustancia vital de la tierra y la vida de los seres vivos depende de ella. Su presencia en la carne es muy significativa, representando algo más del 75 % del peso total de la carne. La retención de agua es fundamental para la jugosidad de la carne y tiene la propiedad de rechazar o fijar cargas tanto positivas como negativas, lo que constituye la base de muchos procesos fisicoquímicos relacionados con la industria de la carne.
+        .col-lg-3
+          img(src='@/assets/curso/temas/t1/16.svg', alt='')
+        .col-lg-5
+          p El contenido de agua en la carne está relacionado con el contenido de proteínas, ambos mantienen una relación directamente proporcional, lo que significa que, a mayor contenido de agua habrá mayor contenido de proteína. Dicha relación tiene una proporcionalidad de 3.6 partes de agua por cada parte de proteína (3.6 a 1). Además, el contenido de proteína y agua en la carne es inversamente proporcional al contenido de grasa. Esto significa que a medida que aumenta la cantidad de grasa en la carne, disminuye el porcentaje de agua y proteínas presentes. 
+
+      .bg3.p-3.px-lg-5.mb-5(data-aos="fade-down")
+        .row.justify-content-center.align-items-center
+          .col-lg-4
+            img(src='@/assets/curso/temas/t1/17.svg', alt='')
+          .col-lg-8
+            p Se dice que el agua en la carne se encuentra presente en tres formas:
+            ul.lista-ul.fa-lista.mb-0
+              li.mb-1 
+                i.lista-ul__vineta
+                | El agua de constitución que representa el 4 % del agua total, es la que se encuentra ligada químicamente a las proteínas. 
+              li.mb-1 
+                i.lista-ul__vineta
+                | El agua de interface que corresponde al agua formada por capas en las fibras musculares y a aquella que se encuentra distante de las proteínas. 
+              li.mb-0 
+                i.lista-ul__vineta
+                | El agua normal, que es aquella que circula libremente en el musculo o está retenida en este a través de las proteínas de gel. Esta forma de agua desaparece en su mayoría cuando la carne es sometida a procesos térmicos como la cocción. 
+
+      .subt.mb-4(data-aos="fade-right")
+        .row.align-items-center
+          .col-auto
+            img(src='@/assets/curso/temas/t2/subt.svg', alt='')
+          .col-auto
+            h3.mb-0.text-white B. Proteínas
+      
+      .row.justify-content-center.align-items-center.mb-4
+        .col-lg-8(data-aos="fade-right")
+          p El mayor beneficio que obtiene el hombre al consumir la carne de los animales se debe a la ingesta de proteínas. Estas cumplen un papel fundamental en el organismo, ya que participan en la mayoría de las funciones y procesos celulares. Además, las proteínas forman parte de la estructura básica de los tejidos, músculos, tendones y piel, contribuyendo al crecimiento, la reparación y el mantenimiento del cuerpo humano.
+          .bg3.p-3
+            .row.justify-content-center.align-items-center
+              .col-lg-3
+                img(src='@/assets/curso/temas/t1/18.svg', alt='')
+              .col-lg-8
+                p.mb-0 Las proteínas son sustancias complejas que están conformadas por una combinación de elementos como el carbono, el hidrógeno, el oxígeno y el nitrógeno, en su estructura también están presentes otros elementos como el azufre, el hierro y el fósforo. La unidad fundamental de las proteínas son los aminoácidos, estos se unen en una secuencia encadenada para dar lugar a la estructura primaria de las proteínas.
+        .col-lg-4(data-aos="fade-left")
+          .titulo-sexto.color-acento-contenido.mb-3
+            h5 Figura 3.
+            span Estructura primaria de las proteínas 
+          figure  
+            img(src='@/assets/curso/temas/t1/fig3.svg', alt='La imagen presenta una cadena de aminoácidos formando la estructura primaria de la proteína. Los aminoácidos se presentan como círculos azules conectados entre sí en una estructura lineal con curvas en varias direcciones. Unas flechas señalan algunos de estos círculos, indicando que cada uno de ellos corresponde a un aminoácido individual dentro de la cadena. ')  
+          figcaption #[b Fuente:] Tomada de LabXchange, (2020 ). 
+    
+      p.mb-4 Las proteínas musculares de la carne se clasifican en tres grupos:
+
+      .row.justify-content-center.mb-5
+        .col-lg-9(data-aos="fade-right")
+          AcordionA(tipo="a" clase-tarjeta="tarjeta bg-color-primario")
+            .row.justify-content-center.text-white(titulo="Proteínas del estroma")
+              .col-md-11.col-12
+                p Este tipo de proteína es el más abundante y está presente en el tejido conectivo del músculo, aunque algunas de estas proteínas tienen efectos negativos en la estabilidad de los productos cárnicos. 
+                p Una proteína importante dentro de este grupo es el colágeno, responsable de la dureza de la carne. Es la proteína más común que se encuentra en la carne y es a través de esta que se transmite la fuerza a los músculos, permitiéndoles realizar las contracciones necesarias para el movimiento. 
+                p La cantidad de colágeno presente en un músculo depende de la cantidad de tejido conectivo que este posea. Generalmente, los músculos que están asociados al movimiento del animal son los que tienen un mayor tejido conectivo, esto se traduce en que los músculos más activos presentan un mayor contenido de colágeno.
+                p Otra proteína que está dentro del grupo de las proteínas del estroma, se conoce con el nombre de elastina, siendo uno de los componentes del tejido conectivo que junto con el colágeno que se encuentran esencialmente en las paredes de las arterias y en los ligamentos. Esta proteína realmente no tiene mucho valor nutricional para el ser humano, tratándose de una proteína baja en aminoácidos y que es resistente a la acción de los ácidos digestivos del cuerpo humano. 
+            .row.justify-content-center.text-white(titulo="Proteínas sarcoplásmicas")
+              .col-md-11.col-12
+                p En este grupo de proteínas se encuentra la denominada mioglobina, la cual es la más abundante en esta categoría. La mioglobina es la responsable de proporcionar el característico color rojizo de la carne, ya que representa el 90 % de los pigmentos que determinan su coloración. 
+                p Además, aunque en menor proporción, la hemoglobina también contribuye al color de la carne, con una participación del 10 % de los pigmentos presentes.
+            .row.justify-content-center.text-white(titulo="Proteínas miofibrilares")
+              .col-md-11.col-12
+                p Este grupo de proteínas son las encargadas de regular las contracciones del tejido muscular y a su vez se divide en dos grupos:
+                ul.lista-ul--color.dark.fa-lista.mb-0
+                  li
+                    i.fas.fa-check-circle
+                    span.mb-0 #[b Las proteínas contráctiles:] en este grupo se encuentran dos proteínas individuales llamadas miosina y actina que están involucradas directamente en la contracción de los músculos permitiendo el movimiento de los animales. 
+                  li
+                    i.fas.fa-check-circle
+                    span.mb-0 #[b Las proteínas reguladoras de las contracciones:] en este grupo están las troponinas y las tropomiosinas, las cuales tienen como función principal regular el proceso de contracción, controlando el actuar de la miosina y de la actina. Tienen la propiedad de ser solubles en soluciones salinas, además de ser las responsables de facilitar la emulsión de la grasa y el agua durante el proceso de cocción de la carne.  
+        .col-lg-3(data-aos="fade-left")
+          img(src='@/assets/curso/temas/t1/19.png', alt='')
+
+      .subt.mb-4(data-aos="fade-right")
+        .row.align-items-center
+          .col-auto
+            img(src='@/assets/curso/temas/t2/subt.svg', alt='')
+          .col-auto
+            h3.mb-0.text-white C. Grasas
+      
+      
+      
+      .subt.mb-4(data-aos="fade-right")
+        .row.align-items-center
+          .col-auto
+            img(src='@/assets/curso/temas/t2/subt.svg', alt='')
+          .col-auto
+            h3.mb-0.text-white D. Carbohidratos
+      
+      
+      
+      .subt.mb-4(data-aos="fade-right")
+        .row.align-items-center
+          .col-auto
+            img(src='@/assets/curso/temas/t2/subt.svg', alt='')
+          .col-auto
+            h3.mb-0.text-white E. Vitaminas
+      
+      
+      
+      .subt.mb-4(data-aos="fade-right")
+        .row.align-items-center
+          .col-auto
+            img(src='@/assets/curso/temas/t2/subt.svg', alt='')
+          .col-auto
+            h3.mb-0.text-white F. Minerales
+
+      .row.justify-content-center.align-items-center
+        .col-lg-1(data-aos="fade-right")
+          img(src='@/assets/curso/temas/t1/25.svg', alt='')
+        .col-lg-11(data-aos="fade-left")
+          p Las carnes de origen animal tienen un rico contenido de minerales como el hierro y el fósforo, sin embargo, el aporte de calcio no es significativo. Pueden contribuir en la dieta del ser humano con cantidades aceptables de potasio y magnesio.
+
+
+      Separador
+
+      #t_1_3.titulo-segundo.color-acento-contenido(data-aos='fade-right')
+        h2 1.3 Características sensoriales de la carne
+
+      p Las características sensoriales de la carne son importantes, dado que permiten la identificación de posibles alteraciones. Además, son la manera como el consumidor identifica al producto y lo califica de acuerdo con sus gustos y/o preferencias (Jurado – Gámez & Insuasty – Santacruz, 2021). 
 
 
   </template>
