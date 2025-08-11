97221c5ed379da85743fb311dbc0931b40bba6a0
Andrea Botello <andrea.botellor@misena.edu.co>
2025-08-11 14:19:34 -0500

    Hallazgos Repo 3.0

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -22,7 +22,7 @@
             .col-lg-12
               figure
                 .video
-                  iframe(width="560" height="315" src="https://www.youtube.com/embed/axphnIkTYY8?si=bMVY0njrdCDuTeKi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+                  iframe(width="560" height="315" src="https://www.youtube.com/embed/FDMhhU2EH3Y?si=bMVY0njrdCDuTeKi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
               figcaption #[b Video.] Generalidades de la carne y sus derivados - introducción
 
 
